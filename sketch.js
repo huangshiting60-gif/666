b9566a9f25dbc8dc39cb752dde47f1e6e6881828
huangshiting60-gif <huangshiting60@gmail.com>
2026-04-21 09:54:22 +0800

    1.0攝影機擷取影像

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let capture;
+
+function setup() {
+  // 第一步驟：產生一個全螢幕的畫布
+  createCanvas(windowWidth, windowHeight);
+  
+  // 取得攝影機影像
+  capture = createCapture(VIDEO);
+  
+  // 隱藏預設產生的 HTML 影片元素，因為我們只想把影像繪製在 Canvas 畫布上
+  capture.hide();
+  
+  // 將影像的繪製模式設定為 CENTER，這樣座標就會從影像的正中心點計算，方便置中
+  imageMode(CENTER);
+}
+
+function draw() {
+  // 設定畫布的背景顏色為 #e7c6ff
+  background('#e7c6ff');
+  
+  // 計算影像的顯示大小為整個畫布寬高比例的 60%
+  let imgWidth = width * 0.6;
+  let imgHeight = height * 0.6;
+  
+  // 將擷取的攝影機影像繪製在畫布的正中間
+  image(capture, width / 2, height / 2, imgWidth, imgHeight);
+}
+
+// 額外加入這個函式：當瀏覽器視窗大小改變時，畫布也會自動調整以維持全螢幕
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
